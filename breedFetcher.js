6f01346893cea66a4a717729b8d89fa8e4367e73
Brenon Parry <brenonparry@gmail.com>
2021-12-03 02:36:56 +0000

    Added index file and updated code on breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,25 @@
 const request = require('request');
 
-const arr = process.argv.slice(2);
-const catBreed = arr[0];
+// const catBreed= process.argv[2]
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, body) => {
-  if (!error)  { // !error = good! It works!
-    const data = JSON.parse(body.body);
-    if (data.length === 0) {
-      console.log(`Woops! Looks like the ${catBreed} got away from us. Maybe try psspsspss?`)
+const fetchBreedDescription = function(catBreed, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, body) => {
+
+    if (!error)  { // !error = good! It works!
+      const data = JSON.parse(body.body);
+      if (data.length === 0) {
+        callback(`Woops! Looks like the ${catBreed} got away from us. Maybe try psspsspss?`, null);
+      } else {
+        callback(null, data[0].description);
+      }
     } else {
-    console.log(data[0].description);
-  };
-} else {
-  console.log("Woops! Looks like the website doesnt exist :( The details are below:")
-  console.log(error);
-}
-  //   console.log(`Woops! Looks like the ${catBreed} got away from us. Maybe try psspsspss?`)
-  // }
+      callback("Woops! Looks like the website doesnt exist :( The details are below:");
+      callback(error, null);
+    }
+  });
+};
 
-  // const data = JSON.parse(body);
-  // console.log(data);
 
-  // if (catBreed === undefined ) {
-  //   console.log(`Woops! Looks like the ${catBreed} got away from us. Maybe try psspsspss?`)
-  // }
-  
-  // const catDescription = data[0].description;
-  
-
-  // console.log(`${catDescription}`)
-  
-});
+module.exports = { fetchBreedDescription };
 
